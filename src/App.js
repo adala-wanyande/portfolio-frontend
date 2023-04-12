5b60bb83347ff48ac417f18ad02156a7c8d0b3ac
benard-dev <benardwanyande@gmail.com>
2023-04-12 10:06:48 +0300

    Add responsive navigation bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
+import NavigationBar from './components/NavigationBar';
+import "./App.css"
 
 const App = () => {
-  
+  return <NavigationBar></NavigationBar>
 }
 
 export default App;
