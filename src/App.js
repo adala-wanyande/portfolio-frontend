52aa2e9ebb0e69ceeafd98f9de6831accafecbbc
benard-dev <benardwanyande@gmail.com>
2023-04-12 12:53:25 +0300

    Add socials component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import InternalNavigationBar from './components/InternalNavigationBar'
+import Socials from './components/Socials'
 import "./App.css"
 
 const App = () => {
-  return <InternalNavigationBar/>
+  return <Socials/>
 }
 
 export default App;
