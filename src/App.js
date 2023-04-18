49ee8f08d5a9368742520529d7f0a6270b5ac2af
benard-dev <benardwanyande@gmail.com>
2023-04-18 08:01:59 +0300

    Add useEffect and useState hooks to App.js component
    Add appropriate hooks to App.js component in order to make API calls to JSON server.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import StickyContainer from './StickyContainer'
 import ScrollableContainer from './ScrollableContainer'
 import "./App.css"
 
 const App = () => {
+  const [projects, setProjects] = useState([])
+  const [blogs, setBlogs] = useState([])
+  const [workHistory, setWorkHistory] = useState([])
+
+  useEffect(() => {
+    fetch('https://localhost:4000/projects')
+      .then(response => response.json())
+      .then(data => setProjects(data))
+  }, [])
+
+  useEffect(() => {
+    fetch('https://localhost:4000/blogs')
+      .then(response => response.json())
+      .then(data => setBlogs(data))
+  }, [])
+
+  useEffect(() => {
+    fetch('https://localhost:4000/work-history')
+      .then(response => response.json())
+      .then(data => setWorkHistory(data))
+  }, [])
+
   return (
     <div className="flex flex-col md:flex-row w-full justify-center">
       <StickyContainer/>
-      <ScrollableContainer/>
+      <ScrollableContainer projects={projects} blogs={blogs} workHistory={workHistory}/>
     </div>
   )
 }
