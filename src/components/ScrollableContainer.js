8b6a2bec8616f0b2fd9275d5482036304ea48167
benard-dev <benardwanyande@gmail.com>
2023-04-18 11:20:15 +0300

    Refactor code in scrollable container component
    In this version, I've:
    
    Added default values for the projects, blogs, and workHistory props so that the component doesn't break if they're not passed in.
    Used the ternary operator to conditionally render the renderProjects, renderBlogs, and renderWorkHistory variables.
    Removed the curly braces around the JSX inside the return statement since they're not necessary when rendering a single element.
    Added self-closing tags to the Subtitle, ProjectItem, BlogItem, Skill, and NavigationButton components. This is optional but considered good practice.

--- src/components/ScrollableContainer.js
+++ src/components/ScrollableContainer.js
@@ -7,49 +7,40 @@ import Skill from './Skill';
 import BlogItem from './BlogItem';
 import CardPlaceholder from './CardPlaceholder'; 
 
-const ScrollableContainer = ({projects, blogs, workHistory}) => {
-  let renderProjects = <CardPlaceholder />;
-  let renderBlogs = <CardPlaceholder />;
-  let renderWorkHistory = <CardPlaceholder />;
-
-  if (projects && blogs && workHistory) {
-    renderProjects = 
-      projects.map((projectObject, index) => {
-        return (
-          <ProjectItem key={`project-${index}`} projectObject={projectObject}></ProjectItem>
-        );
-      });
-
-    renderBlogs = 
-      blogs.map((blogObject, index) => {
-        return (
-          <BlogItem key={`blog-${index}`} blogObject={blogObject}></BlogItem>
-        );
-      });
-
-    renderWorkHistory =
-      workHistory.map((workHistoryObject, index) => {
-        return (
-          <WorkHistoryItem key={`workhistory-${index}`} workHistoryObject={workHistoryObject}></WorkHistoryItem>
-        );
-      });
-  }
+const ScrollableContainer = ({ projects = [], blogs = [], workHistory = [] }) => {
+  const renderProjects = projects.length > 0
+    ? projects.map((projectObject, index) => (
+        <ProjectItem key={`project-${index}`} projectObject={projectObject} />
+      ))
+    : <CardPlaceholder />;
+    
+  const renderBlogs = blogs.length > 0
+    ? blogs.map((blogObject, index) => (
+        <BlogItem key={`blog-${index}`} blogObject={blogObject} />
+      ))
+    : <CardPlaceholder />;
+    
+  const renderWorkHistory = workHistory.length > 0
+    ? workHistory.map((workHistoryObject, index) => (
+        <WorkHistoryItem key={`workhistory-${index}`} workHistoryObject={workHistoryObject} />
+      ))
+    : <CardPlaceholder />;
 
   return (
     <div className="overflow-y-auto flex flex-col">
-      <Subtitle text='Work History' id="work-history"></Subtitle>
+      <Subtitle text='Work History' id="work-history" />
       {renderWorkHistory}
-      <Subtitle text='Projects' id="projects"></Subtitle>
+      <Subtitle text='Projects' id="projects" />
       {renderProjects}
       {/* <div className="flex justify-end mx-4">
         <NavigationButton buttonText="View All Projects" link="https://dev.to/benard-dev"></NavigationButton>
       </div> */}
-      <Subtitle text='Skills' id="skills"></Subtitle>
-      <Skill></Skill>
-      <Subtitle text='Blogs' id="blogs"></Subtitle>
+      <Subtitle text='Skills' id="skills" />
+      <Skill />
+      <Subtitle text='Blogs' id="blogs" />
       {renderBlogs}
       <div className="flex justify-end mx-4 mb-4">
-        <NavigationButton buttonText="View All Blogs" link="https://dev.to/benard-dev"></NavigationButton>
+        <NavigationButton buttonText="View All Blogs" link="https://dev.to/benard-dev" />
       </div>
     </div>
   );
